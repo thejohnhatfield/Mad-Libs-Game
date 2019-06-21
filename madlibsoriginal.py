4b8b3097543af77c2f085dfab9e157f668b0f8d7
John Hatfield <51135089+thejohnhatfield@users.noreply.github.com>
2019-06-20 21:35:18 -0400

    Add files via upload

---
+++ madlibsoriginal.py
@@ -0,0 +1,19 @@
+
+
+name = input("Enter a name: ")
+theme = input("Enter a theme: ")
+a_place = input("Enter a place: ")
+day_of_the_week = input("Enter a day of the week: ")
+time = input("Enter a time: ")
+verb = input("Enter a verb: ")
+animal = input("Enter a/an animal: ")
+body_part = input("Enter a celebrity: ")
+
+print(name + " is having a")
+print(theme + " party!")
+print("It's going to be at " + a_place)
+print("on " + day_of_the_week)
+print("Please make sure to show up at " + time)
+print(", or else you will be required to " + verb)
+print("a/an " + animal)
+print("with your " + body_part)
